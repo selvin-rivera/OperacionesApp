eb65f364f4b2229a6f07eeadfd06e090d985f272
selvin-rivera <nrivera.clements@gmail.com>
2024-06-01 21:52:57 -0600

    creando colores botones

--- OperacionesApp/Tabbed/AreaCirculo.xaml.cs
+++ OperacionesApp/Tabbed/AreaCirculo.xaml.cs
@@ -6,4 +6,24 @@ public partial class AreaCirculo : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnCalcular_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            double circulo = double.Parse(txtN1.Text) * double.Parse(txtN2.Text) * 3.14159;
+            txtResultado.Text = circulo.ToString();
+        }
+        catch (Exception ex)
+        {
+            txtResultado.Text = ex.Message;
+        }
+    }
+
+    private void btnLimpiar_Clicked(object sender, EventArgs e)
+    {
+        txtN1.Text = "";
+        txtN2.Text = "";
+        txtResultado.Text = "";
+    }
 }
--- OperacionesApp/Tabbed/AreaTriangulo.xaml.cs
+++ OperacionesApp/Tabbed/AreaTriangulo.xaml.cs
@@ -6,4 +6,24 @@ public partial class AreaTriangulo : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnCalcular_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            double triangulo = double.Parse(txtN1.Text) * double.Parse(txtN2.Text)/2;
+            txtResultado.Text = triangulo.ToString();
+        }
+        catch (Exception ex)
+        {
+            txtResultado.Text = ex.Message;
+        }
+    }
+
+    private void btnLimpiar_Clicked(object sender, EventArgs e)
+    {
+        txtN1.Text = "";
+        txtN2.Text = "";
+        txtResultado.Text = "";
+    }
 }
